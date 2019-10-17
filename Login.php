733c157477b5c8b9406469e1ca49e6c302deb6d3
HUGO HUERTA <HUGOFIX@HOTMAIL.COM>
2019-10-17 14:48:05 -0500

    se agrega Bootstrap

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
     <title>Bienvenido</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
   </head>
   <body>
